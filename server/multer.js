e9863d1af89b8a9715753455efd530b3feace41b
Muhammed jasim k <kmuhammed@wallpostdev.com>
2022-12-30 17:52:19 +0530

    bug fix

--- server/multer.js
+++ server/multer.js
@@ -2,7 +2,7 @@ const multer  = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null,  './static/post')
+      cb(null,  '../static/post')
     },
     filename: function (req, file, cb) {
         cb(null,Date.now()+file.originalname)
